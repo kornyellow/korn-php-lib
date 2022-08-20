27846c900a1bad29892395e353f0b1b735efb122
kornyellow <kornkubzaza@gmail.com>
2022-08-20 11:48:08 +0700

    Update

--- KornConfig.php
+++ KornConfig.php
@@ -10,7 +10,7 @@ class KornConfig {
 	public static string $websiteAuthor = "กร โรจน์รัตนปัญญา";
 	public static string $websiteOwner = "kornkubzaza@gmail.com";
 	public static string $defaultTitle = "หน้าหลัก";
-	public static string $defaultDescription = "จำหน่าย ออกแบบ ผลิต ติดตั้ง Cooling Tower ทุกแบบ คุณภาพมาตรฐาน โดยวิศวกรผู้เชี่ยวชาญ ทีมงานของเรายินดี เข้าไปตรวจเช็คที่หน้างาน ประเมินราคาฟรี พร้อมบริการหลังการขาย";
-	public static string $defaultAbstract = "หน้าหลักแสดงเนื้อหาเกี่ยวกับรายละเอียดคูลลิ่งทาวเวอร์";
-	public static string $defaultKeywords = "คูลลิ่งทาวเวอร์,คูลลิ่งทาวเวอร์ทรงกลม ทรงเหลี่ยม,หอระบายความร้อน,ติดตั้งเครื่องคูลลิ่งทาวเวอร์,ซ่อมคูลลิ่งทาวเวอร์,ระบบหล่อเย็น ระบบระบายความร้อน,กบคูลลิ่ง,กบ,กรุงเทพมหานคร";
+	public static string $defaultDescription = "";
+	public static string $defaultAbstract = "";
+	public static string $defaultKeywords = "";
 }
