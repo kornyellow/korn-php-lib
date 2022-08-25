6d93845cab573b5ebdcc46972ec7a60493810c79
kornyellow <kornkubzaza@gmail.com>
2022-08-25 16:01:51 +0700

    Added more method, fix Redirect method

--- utils/KornNetwork.php
+++ utils/KornNetwork.php
@@ -8,7 +8,7 @@ class KornNetwork {
 	private static string $header;
 	private static string $description;
 	private static string $abstract;
-	
+
 	public static function getHeader(): string {
 		return self::$header;
 	}
@@ -27,27 +27,27 @@ class KornNetwork {
 	public static function setAbstract(string $abstract): void {
 		self::$abstract = $abstract;
 	}
-	
+
 	public static function getRequestPath(): string {
 		$requestURI = $_SERVER['REQUEST_URI'];
-		if($requestURI == '/')
+		if ($requestURI == '/')
 			return '';
-		
+
 		return substr($requestURI, 1);
 	}
 	public static function getCurrentDomain(): string {
 		return $_SERVER['HTTP_HOST'];
 	}
 	public static function getCurrentDomainURL(): string {
-		if(self::isHTTPS())
+		if (self::isHTTPS())
 			return 'https://'.self::getHost();
-		
+
 		return 'http://'.self::getHost();
 	}
 	public static function isHTTPS(): bool {
-		if($_SERVER['HTTPS'] ?? false)
+		if ($_SERVER['HTTPS'] ?? false)
 			return true;
-		
+
 		return false;
 	}
 	public static function getHost(): string {
@@ -57,36 +57,39 @@ class KornNetwork {
 		$path      = str_replace(['/', '\\'], DIRECTORY_SEPARATOR, $path);
 		$parts     = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
 		$absolutes = [];
-		foreach($parts as $part) {
-			if($part == '.')
+		foreach ($parts as $part) {
+			if ($part == '.')
 				continue;
-			
-			if($part == '..')
+
+			if ($part == '..')
 				array_pop($absolutes);
 			else
 				$absolutes[] = $part;
 		}
-		
+
 		return implode(DIRECTORY_SEPARATOR, $absolutes);
+	}
+	public static function getRequestMethod(): string {
+		return $_SERVER['REQUEST_METHOD'];
 	}
 	public static function getDocumentRoot(): string {
 		return $_SERVER['DOCUMENT_ROOT'];
 	}
 	public static function isLocalHost(): bool {
-		if(self::getRemoteIP() == '127.0.0.1')
+		if (self::getRemoteIP() == '127.0.0.1')
 			return true;
-		
+
 		return false;
 	}
 	public static function getRemoteIP(): string {
 		$clientIP = $_SERVER['HTTP_CLIENT_IP'];
-		if(filter_var($clientIP ?? '', FILTER_VALIDATE_IP))
+		if (filter_var($clientIP ?? '', FILTER_VALIDATE_IP))
 			return $clientIP;
-		
+
 		return $_SERVER['REMOTE_ADDR'];
 	}
-	#[NoReturn] public static function redirectPage($url, $delay = 0): void {
-		echo '<meta http-equiv="refresh" content="'.$delay.'" url = "'.$url.'" />';
+	#[NoReturn] public static function redirectPage($url = '/', $delay = 0): void {
+		echo '<meta http-equiv="refresh" content="'.$delay.';url='.$url.'" />';
 		exit;
 	}
 }
